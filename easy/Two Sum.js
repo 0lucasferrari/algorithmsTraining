20d1f5fe594baa376c8da34fffb47a0056ffd6e0
0lucasferrari <lucas.p.f.carvalho@gmail.com>
2022-09-17 09:41:27 -0300

    Makes an adjustment to Two Sum challenge

--- easy/Two Sum.js
+++ easy/Two Sum.js
@@ -45,11 +45,12 @@ function twoSum(nums, target) {
     }
     return indexes
 }
+
 /*
-console.log(twoSum([2,7,11,15],9))
-console.log(twoSum([3,2,4],6))
-console.log(twoSum([3,3,6],6))
-console.log(twoSum([2,5,5,11],10))
+console.log(twoSum([2,7,11,15],9)) // expects [ 0, 1 ]
+console.log(twoSum([3,2,4],6)) // expects [ 1, 2 ]
+console.log(twoSum([3,3,6],6)) // expects [ 0, 1 ]
+console.log(twoSum([2,5,5,11],10)) // expects [ 1, 2 ]
 */
 
 function twoSum_2(nums,target) {
